aa6eb2fd7c04452e0563b2d75616d8ab17124db5
sumanth33 <53785304+sumanth33@users.noreply.github.com>
2021-05-16 15:38:08 +0530

    car game

---
+++ car game.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# started = False
+# command = input(">>  ")
+# while True :
+#     
+#     if command =="start" :
+#         if started  :
+#             print(" car has already started ")
+#         else :
+#             print("start the car..")
+#         break
+#     elif command == "stop" :
+#         if not started  :
+#             print(" car has not started ")
+#         else :
+#             print("stop the car")
+#         break
+#     elif command == "help" :
+#         print (''' 
+# start>> start the car..
+# stop>> stop the car
+# quit>> exit the game ''')
+#     else :
+#         pass
+#     
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
